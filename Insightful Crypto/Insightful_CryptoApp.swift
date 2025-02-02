e1ac604185fdd374be090686e3655b511a9e3f59
hjjani <hjmusician@gmail.com>
2025-02-02 14:00:18 -0500

    Initial Commit

---
+++ Insightful Crypto/Insightful_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insightful_CryptoApp.swift
+//  Insightful Crypto
+//
+//  Created by Hitanshu Jani on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Insightful_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
