f2fe84698a11c6f7d6770ee7d6b5e8fe96175306
hjjani <141849464+hjjani@users.noreply.github.com>
2025-03-18 17:14:18 -0400

    Update HapticManager.swift

--- Insightful Crypto/Utilities/HapticManager.swift
+++ Insightful Crypto/Utilities/HapticManager.swift
@@ -4,14 +4,19 @@
 //
 //  Created by Hitanshu Jani on 3/17/25.
 //
+// The HapticManager class is responsible for triggering haptic feedback notifications.
+// It uses the UINotificationFeedbackGenerator to provide feedback of different types such as success, error, or warning.
 
 import Foundation
 import SwiftUI
 
 class HapticManager {
-    
+
+    // Static instance of UINotificationFeedbackGenerator to handle haptic feedback.
     static private let generator = UINotificationFeedbackGenerator()
-    
+
+    // Function to trigger a specific type of notification feedback.
+    // - parameter type: The type of notification feedback (success, error, warning).
     static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
         generator.notificationOccurred(type)
     }
