0c3a4757e7313a3bd6b97edc812076cb993cbe71
hjjani <141849464+hjjani@users.noreply.github.com>
2025-03-18 16:26:03 -0400

    Update HomeStatsView.swift

--- Insightful Crypto/Core/Home/Views/HomeStatsView.swift
+++ Insightful Crypto/Core/Home/Views/HomeStatsView.swift
@@ -4,16 +4,18 @@
 //
 //  Created by Hitanshu Jani on 2/24/25.
 //
+// Displays the market statistics (market cap, 24h volume, etc) for the app.
 
 import SwiftUI
 
 struct HomeStatsView: View {
     
     @EnvironmentObject private var vm: HomeViewModel
-    @Binding var showPortfolio: Bool
+    @Binding var showPortfolio: Bool // State binding to toggle portfolio view
     
     var body: some View {
         HStack {
+            // Loop over statistics and display each one
             ForEach(vm.statistics) { stat in
                 StatisticView(stat: stat)
                     .frame(width: UIScreen.main.bounds.width / 3)
