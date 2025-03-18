906978d6afdcfcf2f413c88d56fcd98edfc31c8f
hjjani <141849464+hjjani@users.noreply.github.com>
2025-03-18 15:45:38 -0400

    Update XMarkButton.swift

--- Insightful Crypto/Core/Components/XMarkButton.swift
+++ Insightful Crypto/Core/Components/XMarkButton.swift
@@ -4,16 +4,18 @@
 //
 //  Created by Hitanshu Jani on 3/16/25.
 //
+// Simple reusable button component using SwiftUI.
+// Uses @Environment dismiss to close views (e.g. sheets).
 
 import SwiftUI
 
 struct XMarkButton: View {
     
-    @Environment(\.dismiss) var dismiss
+    @Environment(\.dismiss) var dismiss // Access SwiftUI's dismiss environment action
     
     var body: some View {
         Button(action: {
-            dismiss()
+            dismiss() // Dismiss current view when tapped
         }, label: {
             Image(systemName: "xmark")
                 .font(.headline)
